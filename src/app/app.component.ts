cf1ef2c394f3040108b976e5e323eb9cd6da5d83
pratyush <pratyush.singh@glingtech.onmicrosoft.com>
2025-01-29 15:57:37 +0530

    created grid system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GridsComponent } from "./grids/grids.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [GridsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/grids/grids.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grids',
+  templateUrl: './grids.component.html',
+  styleUrls: ['./grids.component.css'],
+})
+export class GridsComponent {
+  gridSize: number = 3; // Default 3x3 grid
+  items: number[] = [];
+
+  constructor() {
+    this.generateGrid();
+  }
+
+  // Generate grid based on user input
+  generateGrid() {
+    const totalBlocks = this.gridSize * this.gridSize;
+    this.items = Array.from({ length: totalBlocks }, (_, i) => i + 1);
+  }
+
+  // Update grid when user changes the input
+  updateGridSize(event: Event) {
+    const input = (event.target as HTMLInputElement).value;
+    const size = parseInt(input, 10);
+    if (size && size > 0) {
+      this.gridSize = size;
+      this.generateGrid();
+    }
+  }
+}
+
